e312cc89efff01c366f82052dad6dcbe43472d72
Adi Rao <adityakrao.projects@gmail.com>
2024-01-28 04:20:21 -0500

    stuff kinda working now

--- phone-flip-standard/src/firebase/firestore/getAlbums.js
+++ phone-flip-standard/src/firebase/firestore/getAlbums.js
@@ -1,21 +1,26 @@
 import firebase_app from "../clientApp";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { getFirestore, doc } from "firebase/firestore";
+import { getData } from "./getData";
 
 const db = getFirestore(firebase_app)
 
-export default async function getAlbums() {
+export async function getAlbums() {
+    const albums = null;
+    const album_ids = null;
+    const error = null;
     try {
         const user = firebase.auth().currentUser;
+        const user_data = await getData('users', user.uid);
+        const album_ids = user_data.result.data().albums;
+
+        const albums = {};
+        for (let i=0; i<album_ids.length; i++) {
+            albums[album_ids[i]] = getData('albums', album_ids[i]).data;
+        }
+
     } catch (e) {
-        error = e;
+        const error = e;
     }
 
-    const album_ids = getDoc('users', user.uid);
-    const albums = {};
-
-    for (i=0; i<album_ids.length; i++) {
-        albums[album_id[i]] = getData('albums', albums[i]);
-    }
-
-    return { albums, error }
+    return { albums, album_ids, error }
 }
