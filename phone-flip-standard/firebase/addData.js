04611056b3e479fe8b727d77ab0d31665de733c5
Adi Rao <adityakrao.projects@gmail.com>
2024-01-28 00:56:55 -0500

    addData and getData functions

---
+++ phone-flip-standard/firebase/addData.js
@@ -0,0 +1,21 @@
+import firebase_app from "../config";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+
+const db = getFirestore(firebase_app)
+
+
+export default async function addData(colllection, id, data) {
+    let result = null;
+    let error = null;
+
+    try {
+        if (id = null) {} else { 
+        result = await setDoc(doc(db, colllection), data, {
+            merge: true,
+        });}
+    } catch (e) {
+        error = e;
+    }
+
+    return { result, error };
+}
--- phone-flip-standard/firebase/commonFunction.js
+++
@@ -1,7 +0,0 @@
-import { collection, doc, addDoc } from "firebase/firestore";
-import {db} from 'clientApp';
-
-function getPages(album_id) {
-    albumRef = doc(db, 'albums', album_id);
-    pageCollection = collection(db, 'pages');
-}
