1735eff444a6afe00445e386e06c72aefd333426
mesropy <lilit.mesropyan@gmail.com>
2024-01-28 02:25:31 -0500

    Add new simple media type cards

--- phone-flip-standard/src/app/components/column.js
+++ phone-flip-standard/src/app/components/column.js
@@ -14,6 +14,9 @@ export default function Column() {
 
     return <div>
         <MediaCard type={"image"} src={"https://media.geeksforgeeks.org/wp-content/uploads/20190506164011/logo3.png"}/>
+        <MediaCard type={"text"} text={"text text text"}/>
+        <MediaCard type={"audio"} src={"https://media.geeksforgeeks.org/wp-content/uploads/20230524142525/gfg_offline_classes_en.mp3"}/>
+        <MediaCard type={"video"} src={"https://assets.codepen.io/6093409/hubspot-video-example.mp4"}/>
         <button> ADD +</button>
     </div>
 }
