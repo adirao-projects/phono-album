cae052675b1cec61aba2948fbfb263c8f37cdf6f
Adi Rao <adityakrao.projects@gmail.com>
2024-01-28 00:50:52 -0500

    Random Auth shit

--- phone-flip-standard/src/app/auth.js
+++ phone-flip-standard/src/app/auth.js
@@ -1,11 +1,27 @@
 import React from "react";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import firebase from "../firebase/clientApp";
+import {createUserWithEmailAndPassword, GoogleAuthProvider, getAuth} from "firebase/auth";
+
 
 const uiConfig = {
     signInSuccessURL: '/',
     signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
 };
+
+
+export default async function signUp(email, password) {
+    let result = null,
+        error = null;
+    try {
+        result = await createUserWithEmailAndPassword(auth, email, password);
+    } catch (e) {
+        error = e;
+    }
+
+    return { result, error };
+}
+
 
 function SignInScreen() {
     return (
--- phone-flip-standard/src/app/layout.js
+++ phone-flip-standard/src/app/layout.js
@@ -1,3 +1,4 @@
+/*
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -14,4 +15,25 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>{children}</body>
     </html>
   );
+}
+*/
+'use client'
+import './globals.css'
+import { AuthContextProvider } from '@/context/AuthContext'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <AuthContextProvider>
+          {children}
+        </AuthContextProvider>
+      </body>
+    </html>
+  )
 }
